257a7a02227e49b5730652e4cb148f197aa3ab36
Sadmaan Al Saad <sadmaan365@gmail.com>
2025-07-04 15:26:51 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Container,Button,Row, Col } from 'react-bootstrap'
-import "./app.css";
 import Menu from './menu.jsx';
 
 function App() {
